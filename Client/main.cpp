027b585e50d55128205bd1733e5c128d4132f12d
AntonioTonchev <36622262+AntonioTonchev@users.noreply.github.com>
2019-02-27 13:03:01 +0100

    Libraries

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,3 +1,11 @@
 /* List of things to be done for the client: 1)Get the IP address of the host 2)Create a socket 3)Establish a connection to the server 4)Send/Receive messages 5)Close the connection to disconnect */
 
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
