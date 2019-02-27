041c514f0a8fed226f73927ad2aff0315050e492
AntonioTonchev <36622262+AntonioTonchev@users.noreply.github.com>
2019-02-27 12:39:19 +0100

    Setup

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,9 +1,3 @@
+/* List of things to be done for the client: 1)Get the IP address of the host 2)Create a socket 3)Establish a connection to the server 4)Send/Receive messages 5)Close the connection to disconnect */
+
 #include <iostream>
-
-using namespace std;
-
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
-}
