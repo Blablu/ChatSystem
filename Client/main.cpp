360840d4279d4046046a9a1eaf56d12309f02601
AntonioTonchev <36622262+AntonioTonchev@users.noreply.github.com>
2019-03-01 12:07:16 +0100

    It works through Visual Studio

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,16 +1,8 @@
 /* List of things to be done for the client: 1)Get the IP address of the host 2)Create a socket 3)Establish a connection to the server 4)Send/Receive messages 5)Close the connection to disconnect */
-#define WIN32_LEAN_AND_MEAN
-
 #include <iostream>
-#include <stdio.h>
-#include <cstdio>
-#include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
 #include <string>
-#include <sys/types.h>
 #include <Ws2tcpip.h>
-#include <errno.h>
 
 using namespace std;
 
@@ -31,7 +23,7 @@ int main(){
     sockaddr_in saddr; // For IPv4 addresses
     saddr.sin_family = AF_INET;
     saddr.sin_port = htons(port);
-    // inet_pton(AF_INET, ipaddr.c_str(), &saddr.sin_addr); // THIS TEXT-TO-BINARY CONVERSION FUNCTION IS NOT WROKING ON WINDOWS 10 APPARENTLY
+    inet_pton(AF_INET, ipaddr.c_str(), &saddr.sin_addr); // THIS TEXT-TO-BINARY CONVERSION FUNCTION IS NOT WROKING IN CODE::BLOCKS
 
     // Connecting to the server
     int cs = connect(sckt, (sockaddr*)&saddr, sizeof(sockaddr_in));
