c97de1e97320f40eec156c22534f8b945c81c72b
Cristian <moldovan_cristian94@yahoo.com>
2019-03-03 14:16:28 +0100

    works (added multithreading)
    This code works in Xcode (native Macintosh IDE for coding in C++ ), but it doesn't work in CodeBlcoks. It also doesn't work in Windows, unfortunately.

--- Server/server.cpp
+++ Server/server.cpp
@@ -6,214 +6,33 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <string>
-#include <pthread.h>
 #include <thread>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <CoreFoundation/CoreFoundation.h>
 
 using namespace std;
 
-void *task1(void *);
 string inputText;
-char buff[256];
+char buff[512];
 int clientSocket;
-pthread_t threadA[3];
-int noThread = 0;
+long bytesRecv;
+sockaddr_in address;
+//CFSocketRef clientSocket;
 
-static int connFd;
-
-int task1(){}
-
-int task1 ()
+void msgHandler(int msgSock = socket(AF_INET, SOCK_STREAM, 0))
 {
-    cout << "Thread No: " << pthread_self() << endl;
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    clientSocket = msgSock;
 
-        int socked = socket(AF_INET, SOCK_STREAM, 0);
-        sockaddr_in address;
-        address.sin_family = AF_INET;
-        address.sin_port = htons(33000);  //host byte order to neework byte order
-        inet_pton(AF_INET, "0.0.0.0", &address.sin_addr);
-
-        ::bind(socked, (struct sockaddr*)&address, sizeof(address));
-
-        if (listen(socked, 5) < 0)
+    do
     {
-        cerr << "Can't listen!";
-        return -3;
-    }
-
-        sockaddr_in client;
-        socklen_t clientSize = sizeof(client);
-        char host[NI_MAXHOST];
-        char svc[NI_MAXSERV];
-
-        while (noThread < 3)
-    {
-        clientSocket = accept(socked, (struct sockaddr*)&client, &clientSize);
-    }
-
-    pthread_create(&threadA[noThread], NULL, task1, NULL);
-        noThread++;
-
-    for(int i = 0; i < 3; i++)
-    {
-        pthread_join(threadA[i], NULL);
-    }
-
-    send(clientSocket, "Hello there! ", 12, 0);
-
-    bzero(buff, 256);
-
-    int r = read(clientSocket, buff, 255);
-    if (r < 0)
-    {
-        cerr << "Error reading from socket! ";
-        //return -5;
-    }
-    else
-    {
-        cout << "Success! \n" << buff;
-        //r = write(clientSocket, "Confirmed! ", 11);
-    }
-
-    //close the socket
-
-
-    memset(host, 0, NI_MAXHOST); //gets the numeric form of the host name
-    memset(svc, 0, NI_MAXSERV); //get the service info from client
-
-    int result = getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
-
-    if (result)
-    {
-        cout << host << " connected on " << svc << endl;
-    }
-    else
-    {
-        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-        cout << host << " connected on " << ntohs(client.sin_port) << endl;
-    }
-    cout << "Close thread " << endl;
-    close(connFd);
-
-    return 0;
-}
-
-
-int main()
-{
-
-    thread startTask1(task1);
-
-
-    /*pthread_t threadA[3];
-
-    //create a socket
-    int socked = socket(AF_INET, SOCK_STREAM, 0);
-    if (socked < 0)
-    {
-        cerr << "Can't create a socket!";
-        return -1;
-    }
-
-
-    //bind the socket to a port/IP
-    sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_port = htons(33000);  //host byte order to neework byte order
-    inet_pton(AF_INET, "0.0.0.0", &address.sin_addr);
-
-    if (bind(socked, (struct sockaddr*)&address, sizeof(address))< 0)
-    {
-        cerr << "Can't bind to IP/port";
-        return -2;
-    }
-
-    if (listen(socked, 5) < 0)
-    {
-        cerr << "Can't listen!";
-        return -3;
-    }
-
-    int noThread = 0;
-
-
-    //accept a call
-    sockaddr_in client;
-    socklen_t clientSize = sizeof(client);
-    char host[NI_MAXHOST];
-    char svc[NI_MAXSERV];
-
-    while (noThread < 3)
-    {
-
-    clientSocket = accept(socked, (struct sockaddr*)&client, &clientSize);
-    if (clientSocket < 0)
-    {
-        cerr << "Problem with client connecting!";
-        return -4;
-    }
-
-    //close(socked);
-
-//    pthread_create(&threadA[noThread], NULL, task1, NULL);
-    //noThread++;
-    }
-
-    for(int i = 0; i < 3; i++)
-    {
-        pthread_join(threadA[i], NULL);
-    }
-
-    send(clientSocket, "Hello there! ", 12, 0);
-
-    bzero(buff, 256);
-
-    int r = read(clientSocket, buff, 255);
-    if (r < 0)
-    {
-        cerr << "Error reading from socket! ";
-        return -5;
-    }
-    else
-    {
-        cout << "Success! \n" << buff;
-        //r = write(clientSocket, "Confirmed! ", 11);
-    }
-
-    //close the socket
-
-
-    memset(host, 0, NI_MAXHOST); //gets the numeric form of the host name
-    memset(svc, 0, NI_MAXSERV); //get the service info from client
-
-    int result = getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
-
-    if (result)
-    {
-        cout << host << " connected on " << svc << endl;
-    }
-    else
-    {
-        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-        cout << host << " connected on " << ntohs(client.sin_port) << endl;
-    }
-
-    //cout<< "Enter your message here: ";
-    //getline(cin, inputText);
-    //int sendText = send(clientSocket, inputText.c_str(), inputText.size(), 0);
-
-    //display message, echo message*/
-
-    while (true)
-    {
-
-        int bytesRecv = recv(clientSocket, buff, 1024, 0);
-        //clear the buffer
-        memset(buff, 0, 1024);
+        //memset(buff, 0, 1024);
 
         //wait for message
+        bytesRecv = recv(clientSocket, buff, 5096, 0);
 
-
-        if (bytesRecv < 0)
+        if (bytesRecv == -1)
         {
             cerr << "There was a connection issue" << endl;
             break;
@@ -224,17 +43,109 @@ int main()
             cout << "The client disconnected" << endl;
             break;
         }
-
-        //display message
-        cout << "Received: " << string(buff, 0, bytesRecv + sizeof(buff)) << endl;
-
+        else
+        {
+            cout << "Received: " << string(buff, 0, bytesRecv + sizeof(buff)) << endl;
+        }
         //resend message
 
         send(clientSocket, buff, bytesRecv + sizeof(buff), 0);
-
     }
+    while (bytesRecv > 0);
+}
+
+int clientHandler()
+{
+
+    int socked = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (socked < 0)
+    {
+        cerr << "Can't create a socket!";
+        return -1;
+    }
+
+    memset(&address, 0, sizeof(address));
+
+    sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_port = htons(33000);  //host byte order to neework byte order
+    inet_pton(AF_INET, "0.0.0.0", &address.sin_addr);
+
+    if (::bind(socked, (struct sockaddr*)&address, sizeof(address))< 0)
+    {
+        cerr << "Can't bind to IP/port";
+        return -2;
+    }
+    if (listen(socked, SOMAXCONN) < 0)
+    {
+        cerr << "Can't listen!";
+        return -3;
+    }
+
+
+    sockaddr_in client;
+    socklen_t clientSize = sizeof(client);
+    char host[NI_MAXHOST];
+    char svc[NI_MAXSERV];
+
+    memset(host, 0, NI_MAXHOST); //gets the numeric form of the host name
+    memset(svc, 0, NI_MAXSERV); //get the service info from client
+
+    int result = getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
+    if (result == 0)
+    {
+        cout << host << " connected on " << svc << endl;
+    }
+    else
+    {
+        inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+        cout << host << " connected on " << ntohs(client.sin_port) << endl;
+    }
+
+    while (true)
+    {
+        clientSocket = accept(socked, (struct sockaddr*)&client, &clientSize);
+        if (clientSocket < 0)
+        {
+            cerr << "Problem with client connecting!";
+            return -4;
+        }
+        ::thread cmsgHandler = thread(msgHandler, clientSocket);
+        cmsgHandler.detach();
+    }
+    return 0;
+}
+
+int main()
+{
+    thread startClientHandler(clientHandler);
+    startClientHandler.join();
+
+
+     /*send(clientSocket, "Hello there! ", 12, 0);
+     bzero(buff, 512);
+     long r = read(clientSocket, buff, 255);
+     if (r < 0)
+     {
+     cerr << "Error reading from socket! ";
+     return -5;
+     }
+     else
+     {
+     cout << "Success! \n" << buff;
+     //r = write(clientSocket, "Confirmed! ", 11);
+     }
+     //close the socket
+
+
+     //cout<< "Enter your message here: ";
+     //getline(cin, inputText);
+     //int sendText = send(clientSocket, inputText.c_str(), inputText.size(), 0);
+     //display message, echo message*/
+
+
     //close socket
-    close(clientSocket);
     return 0;
 
 }
